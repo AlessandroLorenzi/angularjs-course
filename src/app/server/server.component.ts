2fe5510879e3431d3df05254340663fa29eaf523
Alessandro Lorenzi <alessandro@lm-net.it>
2018-03-02 18:48:00 +0100

    Lecture 37: Apply css class dinamically [ngClass]

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,15 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: ['.online {color: white;}']
 })
 export class ServerComponent implements OnInit {
   serverId = 10;
-
+  serverStatus = '';
   getServerStatus(){
     return this.serverStatus;
   }
   getServerStatusColor(){
-     return this.serverStatus === 'online' ? 'lightgreen' : 'pink' ;
+     return this.serverStatus === 'online' ? 'green' : 'pink' ;
   }
 
   constructor() {
