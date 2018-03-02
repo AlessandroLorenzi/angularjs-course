d4a21ec44bada0915251612c46644bbe1831452f
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2018-03-02 21:40:31 +0100

    Lecture 43: Components struct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipiesComponent } from './recipies/recipies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipiesComponent,
   ],
   imports: [
     BrowserModule,
