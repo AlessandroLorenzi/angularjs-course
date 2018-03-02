06f04832b0006fc7d8f8bb773fda7cadf232e3c4
Alessandro Lorenzi <alessandro@lm-net.it>
2018-03-02 19:25:00 +0100

    Working on assign 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  style: ['.blue {background-color: blue;}']
 })
 export class AppComponent {
   title = 'app';
+  passwordVisible = false;
+  timestamps = []
+  counter = 0
+  onDisplayDetails(){
+    this.passwordVisible = true;
+
+    this.timestamps.push({
+      'date': Date.now(),
+      'counter': this.counter
+    });
+    this.counter = this.counter +1 ;
+
+  }
+
 }
