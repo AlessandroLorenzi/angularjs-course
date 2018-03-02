10acc20ad0a88b35886e7a3ea9b420c7e7c7e585
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2018-03-02 20:38:48 +0100

    Competed Assignment #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  style: ['.blue {background-color: blue;}']
+  styles: ['.white {color: white;}']
 })
 export class AppComponent {
   title = 'app';
   passwordVisible = false;
   timestamps = []
   counter = 0
+  getDateColor(counter){
+    return counter >= 5 ? 'blue' : 'white'
+  }
   onDisplayDetails(){
-    this.passwordVisible = true;
-
+    this.passwordVisible = !(this.passwordVisible);
     this.timestamps.push({
       'date': Date.now(),
       'counter': this.counter
     });
     this.counter = this.counter +1 ;
-
   }
 
 }
