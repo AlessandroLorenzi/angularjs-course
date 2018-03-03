8612175b249129643ec4c9f945dd8688d74394ff
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2018-03-03 12:41:28 +0100

    Lecture 47: recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,16 @@
+export Class Recepie {
+        public name: string;
+        public description: string;
+        public imagePath: string;
+
+        constructor(
+                name: string,
+                desc: string,
+                imagePath: string,
+        ){
+                this.name =  name;
+                this.description = desc;
+                this.imagePath = imagePath;
+        }
+
+}
