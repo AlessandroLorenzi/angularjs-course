4ffd29c6261460672a2c5d8656d47ac1d0ea75c1
Alessandro Lorenzi <alessandro@lm-net.it>
2018-03-11 00:27:33 +0100

    Lecture 79

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,7 +7,7 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  recipe: Recipe = new Recipe('a', 'b', 'b');
+  @Input() recipe: Recipe;
   constructor() {
   }
 
