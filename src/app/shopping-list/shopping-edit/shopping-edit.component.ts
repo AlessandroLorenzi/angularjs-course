41602e706fe80f63dffa86ed57fe8288fcdb15a0
Alessandro Lorenzi <alessandro@lm-net.it>
2018-03-15 14:16:45 +0100

    Lecture 81: Add ingredients to shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() newIngredient = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  resetForm(name, amount) {
+    name.value = '';
+    amount.value = '';
+  }
+
+  onAddIngredient(name, amount) {
+	this.newIngredient.emit(
+		new Ingredient(
+			name.value, amount.value
+		)
+	);
+    this.resetForm(name, amount);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,7 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
   ingredients: Ingredient[] = [
     new Ingredient('apple', 3),
     new Ingredient('shugar', 150),
@@ -15,6 +16,10 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onNewIngredient(newIngredient){
+    this.ingredients.push(newIngredient);
   }
 
 }
